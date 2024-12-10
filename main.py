2db04cfaa6c555a71236dff704dbfa1443cc1c30
bytulka115 <gomenukoleg65@gmail.com>
2024-12-10 18:27:21 +0200

    123
    132

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ class Player:
         self.hitbox.x = x
         self.hitbox.y = y
         self.speed = speed
+        self.bullets = []
+
+
 
     def draw(self, window):
         window.blit(self.texture, self.hitbox)
+        for bullet in self.bullets:
+            bullet.draw(window)
+
 
 
 
@@ -31,6 +37,50 @@ class Player:
 
         if keys[pygame.K_UP]:
             self.hitbox.y -= self.speed
+        if keys[pygame.K_v]:
+            self.bullets.append(Bullet("bullet (1).png",
+                                       10,10,
+                                       self.hitbox.x + 28, self.hitbox.y,
+                                       10))
+
+
+
+
+        for bullet in self.bullets:
+            bullet.move()
+
+
+
+
+class Bullet:
+    def __init__(self, skin, width, height, x, y, speed):
+        self.texture = pygame.image.load(skin)
+        self.texture = pygame.transform.scale(self.texture, [width, height])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+
+
+
+
+    def move(self):
+        self.hitbox.y -= self.speed
+
+
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -40,18 +90,20 @@ class Player:
 
 
 class Enemy:
-    def __init__(self, width, height, x, y, skin):
+    def __init__(self, speed, width, height, x, y, skin):
         self.texture = pygame.image.load(skin)
         self.texture = pygame.transform.scale(self.texture, [width, height])
         self.hitbox = self.texture.get_rect()
         self.hitbox.x = x
         self.hitbox.y = y
-
+        self.speed = speed
 
     def draw(self, window):
         window.blit(self.texture, self.hitbox)
 
 
+    def move(self):
+        self.hitbox.y += self.speed
 
 
 
@@ -96,8 +148,8 @@ class Enemy:
 pygame.init()
 window = pygame.display.set_mode([700, 500])
 fps = pygame.time.Clock()
-player = Player(3, 65, 85, 147, 129, "rocket (1).png")
-enemy = Enemy(55, 45, 147, 129, "ufo (1).png")
+player = Player(3, 65, 85, 250, 400, "rocket (1).png")
+enemy = Enemy(5, 55, 45, 147, 129, "ufo (1).png")
 background = pygame.image.load("galaxy (1).jpg")
 background = pygame.transform.scale(background, [700, 500])
 game = True
@@ -108,7 +160,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-
+    enemy.move()
     player.move()
     window.fill([123, 123,  123])
     window.blit(background,[0, 0])
